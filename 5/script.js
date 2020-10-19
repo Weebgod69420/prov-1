8522212e964fe5c53b8e8619f06e49b2de9f5b9d
Jonathan Lönnqvist <jonathan.lonnqvist@skola.botkyrka.se>
2020-10-19 15:13:17 +0200

    6

---
+++ 5/script.js
@@ -0,0 +1,6 @@
+// där x är basen, y är höjden och z = 2
+function areaOfTriangle(x, y, z){
+x*y/z
+}
+
+return areaOfTriangle(10, 5, 2)
---
+++ 6/script.js
@@ -0,0 +1,10 @@
+const email1 = "blabla@gmail.com"
+const email2 = "blabla@gmail.com"
+
+function isSameUser (email1, email2){
+if (email1 == email2){
+return "true"
+}
+}
+
+isSameUser(email1, email2)
