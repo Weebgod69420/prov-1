eaf8ce4d5f097c40c686179a77c9a59cb71e8dc1
Jonathan Lönnqvist <jonathan.lonnqvist@skola.botkyrka.se>
2020-10-19 15:39:46 +0200

    1o

---
+++ 9/script.js
@@ -0,0 +1,8 @@
+let array = ["jonte", "marcus", "einar"]
+
+function nameCap(array){
+    // kommer inte ihåg uppercase commandot och tror inte att jag hinner hitta det
+array = ["JONTE", "MARCUS", "EINAR"]
+}
+
+return nameCap(array)
