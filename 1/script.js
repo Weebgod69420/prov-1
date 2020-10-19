c0b709553b413ab185ca2be171003a8d72977a6d
Jonathan Lönnqvist <jonathan.lonnqvist@skola.botkyrka.se>
2020-10-19 15:07:12 +0200

    5

---
+++ 1/script.js
@@ -0,0 +1,3 @@
+const myName = "Jonathan"
+const isHuman = "maaaaayyyybe"
+let myAge = 16
---
+++ 4/script.js
@@ -0,0 +1,9 @@
+const array = ["first element", "las element"]
+
+function lastElement(array){
+array(2)
+}
+
+return lastElement()
+
+// jag e inte bra på dehär asså
