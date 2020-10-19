7d86326897645c21f8493fd8db0fa3d52989faf3
Jonathan Lönnqvist <jonathan.lonnqvist@skola.botkyrka.se>
2020-10-19 15:26:26 +0200

    8

---
+++ 7/script.js
@@ -0,0 +1,8 @@
+let name = "stockholm"
+
+function makeCity(name){
+    let name = stockholm
+    let stockholm = [population = 0, "stockholm"]
+}
+
+return makeCity(stockholm)
---
+++ 8/script.js
@@ -0,0 +1,2 @@
+// Can something ever truly be random? 
+// In protest i refuse to do this. That is the only reason cuz i totally could if i wanted to
